4f6902c1ca08d0a79a1df9df9a43706ded7637a3
Adithi H <adithih03@gmail.com>
2024-10-16 15:04:35 +0530

    project setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>text</h2>
+      <Button>YAYY</Button>
+    </div>
+  );
+}
