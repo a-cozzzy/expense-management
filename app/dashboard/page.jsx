0b4d1f7f304f022d0006069432839a8c430ad43c
Adithi H <adithih03@gmail.com>
2024-10-18 22:35:46 +0530

    authentication

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <UserButton />
+      <h1>dsfhsdjfgsdj</h1>
+    </div>
+  )
+}
+
+export default Dashboard
