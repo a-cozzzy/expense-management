2ecb468250a232f845520c9aa646252c05e6dc8c
Am3li1 <ameliasantosh2304@gmail.com>
2024-11-04 23:33:11 +0530

    add_expense

---
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { db } from '@/utils/dbConfig';
+import { Budgets } from '@/utils/schema';
+import { useUser } from '@clerk/nextjs';
+import { eq, sql } from 'drizzle-orm';
+import React, { useEffect } from 'react'
+
+function Expenses({params}) {
+    const {user}=useUser();
+    useEffect(()=>{
+
+        user&&getBudgetInfo();
+    },[user]);
+
+    const getBudgetInfo=async()=>{
+        const result = await db.select({
+            ...getTableColumns(Budgets),
+            totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+            totalItem: sql`count(${Expenses.id})`.mapWith(Number)
+        }).from(Budgets)
+        .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+        .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress)) // Assuming this field exists
+        .where(eq(Budgets.id, params.id))
+        .groupBy(Budgets.id)
+
+        cconsole.log(result);
+    }
+
+    return (
+        <div className='p-10'>
+            <h2 className='text-2xl font-bold'>My Expenses</h2>
+        </div>
+    )
+}
+
+export default Expenses
+
